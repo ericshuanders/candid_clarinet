487fb35e1c9058d765b4f1fcac03e2a2192c6d90
Eric <ericshuanders@gmail.com>
2021-01-11 10:28:47 -0500

    added create comment

--- server/routes/open/index.js
+++ server/routes/open/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { createUser, loginUser } = require('../../controllers/users');
 const { getSpecificPost, getAllPosts } = require('../../controllers/posts');
-
+const { createComment } = require('../../controllers/comments');
 router.post('/', createUser);
 router.post('/login', loginUser);
 router.get('/:id', getSpecificPost);
 router.get('/', getAllPosts);
+router.post('/:id', createComment);
 
 module.exports = router;
